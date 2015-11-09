e72086609bdd9e487ea82de266eb0fdadaa3543f
Eugenio Roman <eugenio.motanum91@gmail.com>
2015-11-09 16:04:35 -0600

    Added all files
    Added all necessary files

---
+++ CustomScenery/Decorators/Type/DecoDecorator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom_Scenery.Decorators.Type
+{
+    class DecoDecorator : IDecorator
+    {
+        public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
+        {
+            go.AddComponent<Deco>();
+
+            if (options.ContainsKey("heightDelta"))
+                (new HeightDecorator((double)options["heightDelta"])).Decorate(go, options, assetBundle);
+        }
+    }
+}
